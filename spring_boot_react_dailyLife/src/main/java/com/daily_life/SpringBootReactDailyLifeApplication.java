2918b40875917c6dafdd33667da7640c633f379a
HANGYESUK <akakakak66@naver.com>
2022-03-18 15:32:10 +0900

    first commit

---
+++ spring_boot_react_dailyLife/src/main/java/com/daily_life/SpringBootReactDailyLifeApplication.java
@@ -0,0 +1,13 @@
+package com.daily_life;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactDailyLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReactDailyLifeApplication.class, args);
+	}
+
+}
