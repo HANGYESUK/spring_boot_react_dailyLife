23f6d003c799b9883a36207e76cc4221038676e0
hope1021 <kdy102130@naver.com>
2022-03-22 17:32:45 +0900

    행복저금통 수정

--- spring_boot_react_dailyLife/src/main/java/com/daily_life/service/IHBService.java
+++ spring_boot_react_dailyLife/src/main/java/com/daily_life/service/IHBService.java
@@ -9,5 +9,6 @@ public interface IHBService {
 	void HBUpdate(HBVO vo); 			// 수정
 	void HBDelete(int hbNo); 			// 삭제
 	ArrayList <HBVO> HBList();			// 목록
+	HBVO HBDetail (int hbNo);		// 상세정보
 	ArrayList <HBVO> HBOpen(int hbNo);	// 개봉
 }
