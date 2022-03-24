be5b35c148dbaa6d945c9dc1d4ec26a1cdc6048d
jihodaddy <lastboom@gmail.com>
2022-03-24 09:52:31 +0900

    행복저금통 백엔드

--- spring_boot_react_dailyLife/src/main/java/com/daily_life/service/IHbBoardService.java
+++ spring_boot_react_dailyLife/src/main/java/com/daily_life/service/IHbBoardService.java
@@ -6,7 +6,7 @@ import com.daily_life.model.HbBoardVO;
 
 public interface IHbBoardService {
 	void hbBoardInsert(HbBoardVO vo); // 등록
-	ArrayList<HbBoardVO> hbBoardList(); // 전체 목록 조회
+	ArrayList<HbBoardVO> hbBoardList(int hbNo); // 전체 목록 조회
 	HbBoardVO hbBoardDetail(int hb_bNo); // 상세 조회
 	String hbBoardHbName(int hbNo); // 행복저금통 이름 불러오기
 }
